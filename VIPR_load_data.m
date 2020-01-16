9562806db1ffa540306c7cb962e67298c2d7e916
Borisfer <58266457+Borisfer@users.noreply.github.com>
2020-01-16 12:23:12 +0200

    Add files via upload

--- VIPR_load_data.m
+++ VIPR_load_data.m
@@ -3,13 +3,13 @@
 [FOV_size,IMG_T] = input_stack;
 IS.FOV_size = FOV_size; % size of ROI used
 
-%% load image coordinates here
+%% load image coordinates here (change to match your data)
 % NFP positions input
 load(['TP images','\NFP_pos_TP_images.mat']);
 z_stack_pos = z_stack_pos';
-%     z is always emitter radius
+% z position of molecule (default to emitter radius)
 z_pos = zeros(length(z_stack_pos),1)+IS.z_emit;
-% xy are 0
+% xy default 0
 xy = zeros(length(z_stack_pos),2);
 
 %
